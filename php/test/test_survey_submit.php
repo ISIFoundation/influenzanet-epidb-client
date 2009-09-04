dac2ccb961fb9ce0f21e1b94931e2caf74bbddf9
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-09-04 22:17:21 +0200

    [php] Update example to use api-key.

--- php/test/test_survey_submit.php
+++ php/test/test_survey_submit.php
@@ -2,9 +2,10 @@
 
 require_once('../EpiDBClient.php');
 
+$key = '0123456789abcdef0123456789abcdef01234567';
 $data = 'data';
 
-$client = new EpiDBClient();
+$client = new EpiDBClient($key);
 $res = $client->survey_submit($data);
 
 print "$res\n";
