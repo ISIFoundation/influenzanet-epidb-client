abee0b016445ef701b6d262dbe1c4955ee2ef659
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-09-06 13:07:15 +0200

    [python] Merge client.py to the module's __init__.py

--- python/epidb/client/__init__.py
+++ python/epidb/client/__init__.py
@@ -1,3 +1,47 @@
 
-from epidb.client.client import EpiDBClient, __version__
+import urllib
+import urllib2
+
+__version__ = '0.0~20090906.1'
+__user_agent__ = 'EpiDB-Client/%s (python)' % __version__
+
+class EpiDBClient:
+
+	version = __version__
+	user_agent = __user_agent__
+
+	server = 'https://egg.science.uva.nl:7443'
+	path_survey = '/survey/'
+
+	def __init__(self, api_key=None):
+		self.api_key = api_key
+
+	def __epidb_call(self, url, param):
+		res = None
+		sock = None
+
+		try:
+			data = urllib.urlencode(param)
+
+			req = urllib2.Request(url)
+			req.add_header('User-Agent', self.user_agent)
+			if self.api_key:
+				req.add_header('Cookie', 'epidb-apikey=%s' % self.api_key)
+			sock = urllib2.urlopen(req, data)
+			res = sock.read()
+			sock.close()
+		except urllib2.HTTPError, e:
+			res = e.read()
+
+		return res
+	
+	def survey_submit(self, data):
+		param = {
+			'data': data
+		}
+
+		url = self.server + self.path_survey
+		res = self.__epidb_call(url, param)
+
+		return res
 
