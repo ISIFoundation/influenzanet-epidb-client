41f14e78e04d4574289526a5e498003e81922bd6
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-09-08 15:32:01 +0200

    [python] Add example: submit intake data.

---
+++ python/examples/submit_intake.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+sys.path += ['..']
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from epidb.client import EpiDBClient
+
+api_key = 'your-epidb-api-key-here'
+user_id = '1c66bb91-33fd-4c6c-9c11-8ddd94164ae8'
+data = {
+    'birth-place': 'Jakarta',
+    'birth-day': '2009-09-09',
+    'has-pets': False
+}
+
+param = json.dumps(data)
+
+client = EpiDBClient(api_key)
+res = client.intake_submit(user_id, param)
+
+result = json.loads(res)
+status = result['stat']
+
+print "status:", status
+
+if status != 'ok':
+    print "error code:", result['code']
+    print "       msg:", result['msg']
+
