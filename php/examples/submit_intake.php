d14164bed5011daf45213bb84c2de09fe13d7af3
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-09-08 15:58:52 +0200

    [php] Add examples: submit survey result and intake data.

---
+++ php/examples/submit_intake.php
@@ -0,0 +1,32 @@
+<?
+
+// This example requires JSON library that is available
+// in PHP 5.2 or later or json PECL package.
+
+require_once('../EpiDBClient.php');
+
+$api_key = 'your-epidb-api-key-here';
+$user_id = '1c66bb91-33fd-4c6c-9c11-8ddd94164ae8';
+$data = array(
+    'birth-place' => 'Jakarta',
+    'birth-day' => '2009-09-09',
+    'has-pets' => False
+);
+
+$param = json_encode($data);
+
+$client = new EpiDBClient($api_key);
+$res = $client->intake_submit($user_id, $param);
+
+$result = json_decode($res);
+$status = $result->stat;
+
+header('Content-Type: text/plain');
+
+print("status: " . $status . "\n");
+
+if ($status != 'ok') {
+    print("error code: " . $result->code . "\n");
+    print("       msg: " . $result->msg . "\n");
+}
+
---
+++ php/examples/submit_survey.php
@@ -0,0 +1,39 @@
+<?
+
+// This example requires JSON library that is available
+// in PHP 5.2 or later or json PECL package.
+
+require_once('../EpiDBClient.php');
+
+$api_key = 'your-epidb-api-key-here';
+$data = array(
+    'user_id' => '1c66bb91-33fd-4c6c-9c11-8ddd94164ae8',
+    'date' => '2009-09-09 09:09:09',
+    'answers' => array(
+        'q1' => 1,
+        'q2' => True,
+        'q3' => array( 1, 2, 3 ),
+        'q4' => 'Jakarta'
+    )
+);
+
+$param = json_encode($data);
+
+$client = new EpiDBClient($api_key);
+$res = $client->survey_submit($param);
+
+$result = json_decode($res);
+$status = $result->stat;
+
+header('Content-Type: text/plain');
+
+print("status: " . $status . "\n");
+
+if ($status == 'ok') {
+    print("id: " . $result->id . "\n");
+}
+else {
+    print("error code: " . $result->code . "\n");
+    print("       msg: " . $result->msg . "\n");
+}
+
