72181de1a679a3df8c5f32fffb026402c92842f7
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-25 13:09:25 +0100

    Add survey_id.

--- python/examples/submit_response.py
+++ python/examples/submit_response.py
@@ -9,6 +9,7 @@ api_key = 'your-epidb-api-key-here'
 data = {
     'user_id': '1c66bb91-33fd-4c6c-9c11-8ddd94164ae8',
     'date': '2009-09-09 09:09:09',
+    'survey_id': 'example-1.0',
     'answers': {
         'q1': 1,
         'q2': True,
