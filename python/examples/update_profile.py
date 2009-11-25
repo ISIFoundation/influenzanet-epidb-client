c547016235228b9e4e40ca9712b48e82fe267a0a
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-25 13:09:05 +0100

    [python] Move source codes to src.

--- python/examples/update_profile.py
+++ python/examples/update_profile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-sys.path += ['..']
+sys.path += ['../src']
 
 from epidb_client import EpiDBClient
 
--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+import sys
+sys.path += ['src']
+
 import epidb_client
 version = epidb_client.__version__
 
@@ -9,6 +12,7 @@ setup(
     url = 'http://www.epiwork.eu/',
     description = 'EPIWork Database - Client Code',
     author = 'Fajran Iman Rusadi',
+    package_dir = {'': 'src'},
     packages = ['epidb_client'],
     install_requires = ['setuptools'],
 )
