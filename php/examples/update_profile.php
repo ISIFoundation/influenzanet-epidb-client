75dfda78c2aabede5f70d859277bde79753d939e
Fajran Iman Rusadi <fajran@gmail.com>
2009-12-15 12:05:10 +0100

    [php] Update examples.

--- php/examples/update_profile.php
+++ php/examples/update_profile.php
@@ -1,17 +1,23 @@
 <?
 
 require_once('../EpiDBClient.php');
+require('config.php');
 
-$api_key = 'your-epidb-api-key-here';
-$user_id = '1c66bb91-33fd-4c6c-9c11-8ddd94164ae8';
-$data = array(
-    'birth-place' => 'Jakarta',
-    'birth-day' => '2009-09-09',
-    'has-pets' => False
-);
+$api_key = $_api_key;
+$user_id = $_user_id;
+$profile_survey_id = $_profile_survey_id;
+$answers = array('birth-place' => 'Jakarta',
+                 'birth-day' => '2009-09-09',
+                 'has-pets' => False);
+
+// If $date is null, the current date and time will be used
+//   $date = null;
+$date = '2009-12-15 01:02:03';
 
 $client = new EpiDBClient($api_key);
-$result = $client->profile_update($user_id, $data);
+$client->server = $_server; // Use this if you want to override
+                            // the server location
+$result = $client->profile_update($user_id, $survey_id, $answers, $date);
 
 $status = $result['stat'];
 
@@ -19,7 +25,10 @@ header('Content-Type: text/plain');
 
 print("status: " . $status . "\n");
 
-if ($status != 'ok') {
+if ($status == 'ok') {
+    print("id: " . $result['id'] . "\n");
+}
+else {
     print("error code: " . $result['code'] . "\n");
     print("       msg: " . $result['msg'] . "\n");
 }
