6b11880792b6222e0aa1cacfb7d7679c3e1f84b8
Klaas van Schelven <klaas@vanschelven.com>
2011-07-16 10:56:48 +0200

    Moved python stuff to root

--- php/examples/config.php
+++
@@ -1,8 +0,0 @@
-<?
-
-$_server = 'http://localhost:8080/';
-$_api_key = '0000000000000000000000000000000000000000';
-$_user_id = '00000000-0000-0000-0000-000000000000';
-$_survey_id = 'dev-response-example-0.0';
-$_profile_survey_id = 'dev-profile-example-0.0';
-
